7c23c3411ef2bd5effac5c1b3705e79b9231afbb
Panchita <franciscalandwehr@hotmail.com>
2025-08-05 13:01:04 -0500

    fixed path

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import NavLinks from '../ui/dashboard/nav-links'
+import SideNav from '../ui/dashboard/sidenav'
+
+export default function Page() {
+
+    return (
+        <>
+            <p>Dashboard Page</p>
+        </>
+    )
+}
